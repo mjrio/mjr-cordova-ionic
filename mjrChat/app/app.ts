0e533b987f053fa23cb90194e0ca52ff3e435a80
Kevin Verelst <kevin.verelst@euri.com>
2016-06-26 12:42:32 +0200

    fixed typings. Webpack error is a know issue

--- mjrChat/app/app.ts
+++ mjrChat/app/app.ts
@@ -3,72 +3,88 @@ import {ViewChild, Component} from '@angular/core';
 import {ionicBootstrap, Platform, MenuController, Nav} from 'ionic-angular';
 import {StatusBar, Splashscreen} from 'ionic-native';
 
+import {FIREBASE_PROVIDERS,
+    defaultFirebase,
+    AngularFire,
+    AuthMethods,
+    AuthProviders,
+    firebaseAuthConfig} from 'angularfire2';
+
 import {LoginPage} from './pages/login/login';
 import {RegisterPage} from "./pages/register/register";
 
 interface PageObj {
-  title: string;
-  component: any;
-  icon: string;
-  index?: number;
+    title: string;
+    component: any;
+    icon: string;
+    index?: number;
 }
 
 @Component({
-  templateUrl: 'build/app.html',
+    templateUrl: 'build/app.html',
 })
 
 class mjrChat {
-  // make HelloIonicPage the root (or first) page
-  rootPage: any = LoginPage;
-  pages: Array<{title: string, component: any}>;
+    // make HelloIonicPage the root (or first) page
+    rootPage:any = LoginPage;
+    pages:Array<{title: string, component: any}>;
 
-  @ViewChild(Nav) nav: Nav;
-  constructor(
-    private platform: Platform,
-    private menu: MenuController
-  ) {
-    this.initializeApp();
+    @ViewChild(Nav) nav:Nav;
 
-    // set our app's pages
-    this.pages = [
-      { title: 'Login', component: LoginPage },
-      { title: 'Register', component: RegisterPage }
-    ];
-  }
+    constructor(private platform:Platform,
+                private menu:MenuController) {
+        this.initializeApp();
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      // The platform is now ready. Note: if this callback fails to fire, follow
-      // the Troubleshooting guide for a number of possible solutions:
-      //
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      //
-      // First, let's hide the keyboard accessory bar (only works natively) since
-      // that's a better default:
-      //
-      // Keyboard.setAccessoryBarVisible(false);
-      //
-      // For example, we might change the StatusBar color. This one below is
-      // good for dark backgrounds and light text:
-      // StatusBar.setStyle(StatusBar.LIGHT_CONTENT)
-    });
-  }
-
-  openPage(page: PageObj) {
-    // the nav component was found using @ViewChild(Nav)
-    // reset the nav to remove previous pages and only have this page
-    // we wouldn't want the back button to show in this scenario
-    if (page.index) {
-      this.nav.setRoot(page.component, {tabIndex: page.index});
-
-    } else {
-      this.nav.setRoot(page.component);
+        // set our app's pages
+        this.pages = [
+            {title: 'Login', component: LoginPage},
+            {title: 'Register', component: RegisterPage}
+        ];
     }
 
-  }
+    initializeApp() {
+        this.platform.ready().then(() => {
+            // The platform is now ready. Note: if this callback fails to fire, follow
+            // the Troubleshooting guide for a number of possible solutions:
+            //
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            //
+            // First, let's hide the keyboard accessory bar (only works natively) since
+            // that's a better default:
+            //
+            // Keyboard.setAccessoryBarVisible(false);
+            //
+            // For example, we might change the StatusBar color. This one below is
+            // good for dark backgrounds and light text:
+            // StatusBar.setStyle(StatusBar.LIGHT_CONTENT)
+        });
+    }
+
+    openPage(page:PageObj) {
+        // the nav component was found using @ViewChild(Nav)
+        // reset the nav to remove previous pages and only have this page
+        // we wouldn't want the back button to show in this scenario
+        if (page.index) {
+            this.nav.setRoot(page.component, {tabIndex: page.index});
+
+        } else {
+            this.nav.setRoot(page.component);
+        }
+
+    }
 }
 
-ionicBootstrap(mjrChat, [], {
-  tabbarPlacement: 'bottom'
+ionicBootstrap(mjrChat, [FIREBASE_PROVIDERS,
+    defaultFirebase({
+        apiKey: "AIzaSyA93TznBXNukAv2WSHnYUUczSNYrJsnx7Q",
+        authDomain: "mjrchat-d0cc6.firebaseapp.com",
+        databaseURL: "https://mjrchat-d0cc6.firebaseio.com",
+        storageBucket: "mjrchat-d0cc6.appspot.com"
+    }),
+    firebaseAuthConfig({
+        provider: AuthProviders.Twitter,
+        method: AuthMethods.Redirect
+    })], {
+    tabbarPlacement: 'bottom'
 });
--- mjrChat/typings/main.d.ts
+++ mjrChat/typings/main.d.ts
@@ -1,1 +1,2 @@
 /// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
+/// <reference path="../firebase3.d.ts" />
